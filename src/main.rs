43af46a09ce5d34b842b534ebd8d3271f1052511
Jake McGrath <thejakezilla80@gmail.com>
2023-11-22 20:40:02 -0600

    Did some stuff.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use crate::claws::Claw;
 
 pub mod claws {
+    use std::fs;
+    use std::path::PathBuf;
     use cursive::{Cursive, CursiveExt};
-    use cursive::views::Dialog;
+    use cursive::views::{Dialog, DummyView, LinearLayout, SelectView};
 
     /// The TUI facilitated by the Claw.
     pub struct Claw {
@@ -23,17 +25,28 @@ pub mod claws {
 
         pub fn main_menu(&mut self) {
             let mut s = &mut self.siv;
-            s.add_layer(
-                Dialog::text("Can you see this dialog window?")
-                    .button("Yes", |s| {
-                        s.add_layer(Dialog::info("YAY!"))
-                    })
-                    .button("No!", |s| {
-                        s.add_layer(Dialog::info("Rut row..."))
-                    })
-                    .button("Quit", |s| s.quit())
-            );
+
+            if let Some(pcap_files) = Self::get_pcap_files() {
+                let mut sel_view = SelectView::<PathBuf>::new();
+                for e in pcap_files.into_iter() {
+                    let f_name = e.file_name().unwrap().to_str().unwrap_or("BadName").to_string();
+                    sel_view.add_item(f_name, e);
+                }
+                let main_layout = LinearLayout::vertical()
+                    .child(sel_view)
+                    .child(DummyView); // TODO: update with ability to run from select options
+
+                s.add_layer(main_layout);
+            } else {
+                s.add_layer(
+                    Dialog::info("No pcap Files Found")
+                );
+            }
             s.run();
+        }
+
+        fn get_pcap_files() -> Option<Vec<PathBuf>> {
+            //TODO
         }
 
         fn read_file(&mut self) {
