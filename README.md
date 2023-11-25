47eca53cc8c6048f4ba270f002c408314e282945
Jake McGrath <jmcgra15@depaul.edu>
2023-11-25 16:37:40 -0600

    Added dependencies & updated the README.md to include the dependencies & some rel. info. Added functionality to the TUI's main menu.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # claws
-A Rust-based Terminal User Interface for packet analysis similar to Wireshark. Named "Claws" because there's no crab-based scary movie involving crabs that I know off the top of my head, and claws rhymes with "Jaws" - so why not?!
+<p>
+A Rust-based Terminal User Interface for packet analysis similar to Wireshark. Named "Claws" because there's no 
+crab-based scary movie involving crabs that I know off the top of my head, and claws rhymes with "Jaws" - so why not?!
+</p>
+<p>
+This project is primarily intended to help me understand the packet capture process. In order to build a rust-based clone
+of Wireshark, this is also intended to help me learn Wireshark too.
+</p>
+<p>
+<b>Note: This is a Debian-based Linux project, and won't work on Windows-based machines.</b>
+</p>
+
+## Dependencies:
+* Rust Crates:
+  * `pcap`: allows access to packet sniffing capabilities of `libpcap`.
+    * `libpcap-dev`: installs the libraries and header files for the `libpcap` library.
+  * `pnet`: allows for the parsing/information extraction of captured packets.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,38 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+use std::process::Command;
 use crate::claws::Claw;
+#[derive(Debug)]
+struct ClawError {
+    msg: String,
+}
+
+impl Display for ClawError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "ClawError: {}", self.msg)
+    }
+}
+
+impl Error for ClawError {}
 
 pub mod claws {
     use std::fs;
     use std::path::PathBuf;
     use cursive::{Cursive, CursiveExt};
-    use cursive::views::{Dialog, DummyView, LinearLayout, SelectView};
+    use cursive::traits::*;
+    use cursive::views::{Button, Dialog, DummyView, LinearLayout, SelectView, TextView};
+
+    const LOGO: &str = "
+
+ ██████╗██╗      █████╗ ██╗    ██╗███████╗
+██╔════╝██║     ██╔══██╗██║    ██║██╔════╝
+██║     ██║     ███████║██║ █╗ ██║███████╗
+██║     ██║     ██╔══██║██║███╗██║╚════██║
+╚██████╗███████╗██║  ██║╚███╔███╔╝███████║
+ ╚═════╝╚══════╝╚═╝  ╚═╝ ╚══╝╚══╝ ╚══════╝
+
+    ";
+
 
     /// The TUI facilitated by the Claw.
     pub struct Claw {
@@ -24,23 +52,38 @@ pub mod claws {
         }
 
         pub fn main_menu(&mut self) {
-            let mut s = &mut self.siv;
+            let capture_items: Vec<(&str, &str)> = vec![
+                ("eth0", "eth0"),
+                ("eth1", "eth1"),
+                ("any", "any"),
+                ("Loopback", "Loopback"),
+            ];
+            let mut s: &mut Cursive = &mut self.siv;
 
-            if let Some(pcap_files) = Self::get_pcap_files() {
-                let mut sel_view = SelectView::<PathBuf>::new();
-                for (f_name, path) in pcap_files.into_iter() {
-                    sel_view.add_item(f_name, path);
+            let logo_view = TextView::new(LOGO);
+            let mut cap_from_file_sel_view = SelectView::<PathBuf>::new()
+                .on_submit(Self::read_file);
+
+            let mut new_capture_sel_view = SelectView::<&str>::new()
+                .on_submit(Self::new_capture);
+            new_capture_sel_view.add_all(capture_items);
+
+            if let Some(cap_files) = Self::get_pcap_files() {
+                for (f_name, path) in cap_files.into_iter() {
+                    cap_from_file_sel_view.add_item(f_name, path);
                 }
-                let main_layout = LinearLayout::vertical()
-                    .child(sel_view)
-                    .child(DummyView); // TODO: update with ability to run from select options
-
-                s.add_layer(main_layout);
             } else {
-                s.add_layer(
-                    Dialog::info("No pcap Files Found")
-                );
+                cap_from_file_sel_view.add_item("No Packet Capture Files Available", PathBuf::default());
             }
+
+            let main_layout = LinearLayout::vertical()
+                .child(logo_view)
+                .child(cap_from_file_sel_view)
+                .child(DummyView)
+                .child(new_capture_sel_view)
+                .child(DummyView)
+                .child(Button::new("Quit", Cursive::quit));
+            s.add_layer(main_layout);
             s.run();
         }
 
@@ -71,13 +114,56 @@ pub mod claws {
                 .filter(|n_p| !n_p.is_empty())
         }
 
-        fn read_file(&mut self) {
-            // TODO: Implement reading from file; possibly by issuing system commands to tcpdump -r
+        /// Takes the file that's provided and parses the contents of the packet capture file.
+        fn read_file(s: &mut Cursive, file: &PathBuf) {
+            let msg: String = format!("You've chosen the read_file function with path: {:?}", file);
+            s.add_layer(Dialog::info(msg))
+        }
+
+        /// Makes a new capture on a given interface.
+        fn new_capture(s: &mut Cursive, capture_type: &str) {
+            let msg: String = format!("You've chosen the new_capture function on capture type: {}", capture_type);
+            s.add_layer(Dialog::info(msg))
         }
     }
 }
 
 fn main() {
+    //install_dependencies();
     let mut claw: Claw = Claw::new();
     claw.main_menu();
+}
+
+/// Installs the dependencies for the project via command execution.
+fn _install_dependencies() {
+    let program_names: Vec<&str> = vec![
+        "libpcap-dev",
+    ];
+    program_names.iter().for_each(|program| {
+        if !is_program_installed(program) {
+            match install_program(program) {
+                Ok(_) => println!("Dependencies '{}' installed", program),
+                Err(e) => eprintln!("{}", e.msg),
+            }
+        } else {
+            println!("Program '{}' already installed...", program);
+        }
+    })
+}
+
+/// Checks to see if the program named `program_name` is installed on the machine.
+fn is_program_installed(program_name: &str) -> bool {
+    Command::new("which")
+        .arg(program_name)
+        .status()
+        .map(|status| status.success())
+        .unwrap_or(false)
+}
+
+/// Installs the program via the `sudo` command using the `apt` program manager.
+fn install_program(program_name: &str) -> Result<(), ClawError> {
+    match Command::new("sudo").arg("apt").arg("install").arg("-y").arg(program_name).status() {
+        Ok(_) => Ok(()),
+        Err(e) => Err(ClawError {msg: format!("unable to install program '{}'\tError: {}", program_name, e)}),
+    }
 }
